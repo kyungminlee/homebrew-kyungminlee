805b2f8bb7e42e244b12f8fdb7741b30eee835aa
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2017-01-04 00:24:50 -0500

    julia-binary: devel version (0.6.0-887815c69d) added.

--- julia-binary.rb
+++ julia-binary.rb
@@ -11,6 +11,17 @@ class JuliaBinary < Formula
     sha256 "77aa539e48b260076d6e0baedb783b90c7478a88755f0671d78c7d8954ec95bf"
   end
 
+  devel do
+    version "0.6.0-887815c69d"
+    if MacOS.prefer_64_bit?
+      url "https://s3.amazonaws.com/julianightlies/bin/linux/x64/0.6/julia-0.6.0-887815c69d-linux64.tar.gz"
+      sha256 "aefc56751b1553bcac1e54fa47e2b166346dc04baad2082a5e599aa8b1b56f57"
+    else
+      url "https://s3.amazonaws.com/julianightlies/bin/linux/x86/0.6/julia-0.6.0-887815c69d-linux32.tar.gz"
+      sha256 "51bfe84f09b066f761c05f6aef966a1f5671a270b5b14e6a83b0a2fd211df8bf"
+    end
+  end
+
   bottle :unneeded
 
   def install
